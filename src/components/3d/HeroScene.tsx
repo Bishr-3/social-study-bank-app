9a2665033ed3cda476041c8216d9b9d6a46cdd4b
wasel <wasel@example.com>
2026-04-21 23:20:03 +0400

    Fix Spline import

--- src/components/3d/HeroScene.tsx
+++ src/components/3d/HeroScene.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useRef } from 'react'
-import Spline from '@splinetool/react-spline/next'
+import Spline from '@splinetool/react-spline'
 import { motion, useScroll, useTransform } from 'framer-motion'
 
 export default function HeroScene() {
